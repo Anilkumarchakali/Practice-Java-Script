980bbc013ade4ba23b913f050089c37a64cd6f7d
Anilkumarchakali <anilkumarmpc0@gmail.com>
2025-02-08 13:02:24 +0530

    typeOfFunction

---
+++ Typesoffunction.js
@@ -0,0 +1,15 @@
+//Function Declaration: Hoisted
+function greet1(name) {
+    return `Hello,${name}`
+}
+console.log(greet1('Anil'));
+//Function Expression: not Hoisted
+let greet2 = function(name) {
+    return `Hello,${name}`
+}
+console.log(greet2('Anil'));
+//Arrow Function: call back
+let greet3 = (name)=> {
+  return `Hell0,${name}`
+}
+console.log(greet3('Java Script'));

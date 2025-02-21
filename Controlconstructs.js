25878b02de8c42e30db61c9b3cd55e343ced3c73
Anilkumarchakali <anilkumarmpc0@gmail.com>
2025-02-21 09:39:47 +0530

    Conctrolconstructs

---
+++ Controlconstructs.js
@@ -0,0 +1,9 @@
+let age = 20;
+if (age >= 18) {
+    console.log("You are eligible to voting.");
+} else if (age >= 16) {
+    console.log("You are eligible to voting.");
+} 
+else {
+    console.log("You are not eligible to voting.");
+}
